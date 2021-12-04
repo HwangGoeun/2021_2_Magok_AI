3d04ab2f2876ee2ff42da24adc3a17a510da37e2
HwangGoeun <hwang_gim@naver.com>
2021-12-04 17:33:29 +0900

    add gstream

--- face_line.py
+++ face_line.py
@@ -33,8 +33,6 @@ def main():
     goeun_image = face_recognition.load_image_file("hwanggim.jpg")
     goeun_face_encoding = face_recognition.face_encodings(goeun_image)[0]
 
-
-
     # Create arrays of known face encodings and their names
     known_face_encodings = [
         obama_face_encoding,
@@ -123,52 +121,83 @@ def main():
     cv2.destroyAllWindows()
 
 ####################################################################################################################
-
+def gstreamer_pipeline(
+    capture_width=160,
+    capture_height=120,
+    display_width=160,
+    display_height=120,
+    framerate=60,
+    flip_method=0,
+):
+    return (
+        "nvarguscamerasrc ! "
+        "video/x-raw(memory:NVMM), "
+        "width=(int)%d, height=(int)%d, "
+        "format=(string)NV12, framerate=(fraction)%d/1 ! "
+        "nvvidconv flip-method=%d ! "
+        "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
+        "videoconvert ! "
+        "video/x-raw, format=(string)BGR ! appsink"
+        % (
+            capture_width,
+            capture_height,
+            framerate,
+            flip_method,
+            display_width,
+            display_height,
+        )
+    )
+ 
+ 
 def linetracing():
-    camera = cv2.VideoCapture(0)
-    camera.set(3,160) 
-    camera.set(4,120)
+    # To flip the image, modify the flip_method parameter (0 and 2 are the most common)
+    print(gstreamer_pipeline(flip_method=0))
+    cap = cv2.VideoCapture(gstreamer_pipeline(flip_method=0), cv2.CAP_GSTREAMER)
+    if cap.isOpened():
+        window_handle = cv2.namedWindow("CSI Camera", cv2.WINDOW_AUTOSIZE)
+        # Window
+        while cv2.getWindowProperty("CSI Camera", 0) >= 0:
+            ret_val, img = cap.read()
+            cv2.imshow("CSI Camera", img)
+           
+            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            blur = cv2.GaussianBlur(gray,(5,5),0)
+           
+            ret,thresh1 = cv2.threshold(blur,130,255,cv2.THRESH_BINARY_INV)
+           
+            mask = cv2.erode(thresh1, None, iterations=2)
+            mask = cv2.dilate(mask, None, iterations=2)
+            cv2.imshow('mask',mask)
+       
+            contours,hierarchy = cv2.findContours(mask.copy(), 1, cv2.CHAIN_APPROX_NONE)
+           
+            if len(contours) > 0:
+                c = max(contours, key=cv2.contourArea)
+                M = cv2.moments(c)
+               
+                cx = int(M['m10']/M['m00'])
+                cy = int(M['m01']/M['m00'])
+               
+                if cx >= 95 and cx <= 125:              
+                    print("Turn Left!")
+                    robot.left(0.8)
+                elif cx >= 39 and cx <= 65:
+                    print("Turn Right")
+                    robot.right(0.8)
+                else:
+                    print("go")
+                    robot.forward(0.8)
+#####################################################################################################3
 
-    while( camera.isOpened() ):
-        ret, frame = camera.read()
-        frame = cv2.flip(frame,-1)
-        cv2.imshow('normal',frame)
-        
-        crop_img =frame[60:120, 0:160]
-        
-        gray = cv2.cvtColor(crop_img, cv2.COLOR_BGR2GRAY)
-    
-        blur = cv2.GaussianBlur(gray,(5,5),0)
-        
-        ret,thresh1 = cv2.threshold(blur,130,255,cv2.THRESH_BINARY_INV)
-        
-        mask = cv2.erode(thresh1, None, iterations=2)
-        mask = cv2.dilate(mask, None, iterations=2)
-        cv2.imshow('mask',mask)
-    
-        contours,hierarchy = cv2.findContours(mask.copy(), 1, cv2.CHAIN_APPROX_NONE)
-        
-        if len(contours) > 0:
-            c = max(contours, key=cv2.contourArea)
-            M = cv2.moments(c)
-            
-            cx = int(M['m10']/M['m00'])
-            cy = int(M['m01']/M['m00'])
-            
-            if cx >= 95 and cx <= 125:              
-                print("Turn Left!")
-                robot.left(0.8)
-            elif cx >= 39 and cx <= 65:
-                print("Turn Right")
-                robot.right(0.8)
-            else:
-                print("go")
-                motor.forward(0.8)
-        
-        if cv2.waitKey(1) == ord('q'):
-            break
-    
-    cv2.destroyAllWindows()
-
-if __name__ == '__main__':
+            # This also acts as
+            keyCode = cv2.waitKey(30) & 0xFF
+            # Stop the program on the ESC key
+            if keyCode == 27:
+                break
+        cap.release()
+        cv2.destroyAllWindows()
+    else:
+        print("Unable to open camera")
+ 
+if __name__ == "__main__":
     main()
